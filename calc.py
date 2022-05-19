162b97f8a251ddb2219db707a673102418656cbf
segzzyy <105700273+segzzyy@users.noreply.github.com>
2022-05-19 12:00:01 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,5 @@
+f_num =int (input ("Enter the first number"))
+s_num =int(input ( "Enter the second number"))
+sum_of_nums = f_num + s_num
+
+print("The sum of the 2 numbers is"+str(sum_of_nums))
---
+++ strings.py
@@ -0,0 +1,20 @@
+
+from logging import setLoggerClass
+from pyexpat import XML_PARAM_ENTITY_PARSING_NEVER
+
+
+fruit="pin_eap_ple"
+print(fruit[5:])
+print(fruit[:2])
+#strip ()removes spaces between words
+f_name = "  Xavier"
+print(f_name.strip())
+f_name= "Xavier" 
+s_name= "Segera" 
+full_name = f_name +s_name
+
+
+city="mombasa"
+print(len(city))
+# int - >
+x=100 #.int
